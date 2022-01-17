949582ad998f578b60d28fa7fe85595690bdd4bb
Ksenia Zhukova <ks.jukova6@gmail.com>
2022-01-17 22:03:25 +0300

    Декоративные элементы внутренних страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('#name');
+const weightInput = document.querySelector('#weight');
+const emailInput = document.querySelector('#email');
+const telInput = document.querySelector('#tel');
+const requiredFields = [nameInput, weightInput, emailInput, telInput];
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +17,27 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+form.addEventListener("submit", ( evt) => {
+  let validated = true
+  evt.preventDefault();
+  requiredFields.forEach((el) => {
+    if (el.value.length === 0) {
+      el.classList.add('form__input-error')
+      validated = false
+    }
+  })
+  if (validated) {
+    form.submit()
+  } else {
+    requiredFields.forEach((el) => {
+      el.addEventListener('change', () => {
+        if (el.classList.contains('form__input-error')) {
+          el.classList.remove('form__input-error')
+        }
+      }, {once: true})
+    });
   }
 });
